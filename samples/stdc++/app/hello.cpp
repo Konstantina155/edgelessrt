28c88536496c0516bc4a69f3851f28d149b28f97
Konstantina Papafragkaki <csd4331@csd.uoc.gr>
2022-12-09 00:04:54 +0200

    RocksDB sample

--- samples/stdc++/app/hello.cpp
+++ samples/stdc++/app/hello.cpp
@@ -1,23 +1,34 @@
-// Copyright (c) Edgeless Systems GmbH.
-// Licensed under the MIT License.
-
 #include <iostream>
 #include <stdexcept>
 
+#include <rocksdb/db.h>
+
+void *aligned_alloc(size_t align, size_t size)
+{
+ return malloc(((void) align, size));
+}
+
 using namespace std;
 
-int main()
-{
-    cout << "hello stdc++\n";
+int main(int argc, char** argv) {
+    rocksdb::DB* db;
+    rocksdb::Options options;
+    options.create_if_missing = true;
+    rocksdb::Status status =
+    rocksdb::DB::Open(options, "/tmp/testdb", &db);
+    assert(status.ok());
 
-    try
-    {
-        throw logic_error("exception test");
-    }
-    catch (const exception& e)
-    {
-        cout << e.what() << '\n';
-    }
+    // Insert value
+    status = db->Put(rocksdb::WriteOptions(), "Test key", "Test value");
+    assert(status.ok());
 
-    return 0;
+    // Read back value
+    std::string value;
+    status = db->Get(rocksdb::ReadOptions(), "Test key", &value);
+    assert(status.ok());
+
+    assert(!status.IsNotFound());
+    // Read key which does not exist
+    status = db->Get(rocksdb::ReadOptions(), "This key does not exist", &value);
+    assert(status.IsNotFound());
 }
